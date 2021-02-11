4005e011d36059ab259b94738dcfa9dacd235186
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-11 08:41:44 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main () {
+int main (int argc, char *argv[]) {
    char command[127];
-   strcpy(command, "lua");
-   system(command);
+   FILE *fp;
+   fp = fopen("data.communication", "w");
+   fprintf(fp, argv[1]);
+   fclose(fp);
    strcpy(command, "python3");
+   system(command);
+   strcpy(command, "lua");
    system(command);
    return(0);
 } 
