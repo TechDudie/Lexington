1c150ee65821a2efd093d0867683ecbde8302463
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-11 08:31:54 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main () {
+   char command[127];
+   strcpy(command, "lua");
+   system(command);
+   strcpy(command, "python3");
+   system(command);
+   return(0);
+} 
