dd2339a277b7bb29815198178a76337ba0bd5283
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-11 09:55:41 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+io = require("io")
+extlib = require("extlib")
 file = io.open("data.communication", "r")
 io.input(file)
 filecont = io.read()
@@ -13,14 +15,19 @@ for str in string.gmatch(code,"([^\n]+)") do
     table.insert(tokens,str)
   end
   if tokens[1] == "OUT.PRINT" then
+    tokens[2] = extlib.check(tokens[2])
     print(tokens[2])
   end
   if tokens[1] == "OUT.FILE" then
+    tokens[2] = extlib.check(tokens[2])
+    tokens[3] = extlib.check(tokens[3])
     file = io.open(tokens[2], "w")
     io.output(file)
     io.write(tokens[3])
     io.close(file)
   end
   if tokens[1] == "OUT.VAR" then
+    tokens[2] = extlib.check(tokens[2])
+    tokens[3] = extlib.check(tokens[3])
     var[tokens[2]] = tokens[3]
 end
