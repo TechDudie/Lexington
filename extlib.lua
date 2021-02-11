ad1b813571a9357e3813c6a34a1c52e03eadb31f
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-11 09:50:29 -0600

    Update extlib.lua

--- extlib.lua
+++ extlib.lua
@@ -1,1 +1,12 @@
-
+function check(token,var)
+  if token:sub(1,1) == "#" then
+    return var[token:sub(2)]
+  end
+  if token:sub(1,1) == "$" then
+    file = io.open(token:sub(2), "r")
+    io.input(file)
+    text = io.read()
+    io.close()
+    return text
+  end
+end
